6c0bddefe25ad97e36e72e3a95740e1efdb977ba
Ben <benjaminctyner@gmail.com>
2023-06-10 23:50:18 -0400

    set up openai api.

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
-import Link from 'next/link';
-import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from 'next/image';
+import Link from 'next/link';
+import { Logo } from '../components/Logo';
+import HeroImage from '../public/hero.webp';
 
 export default function Home() {
-  console.log(user);
   return (
-    <div>
-      <h1 className='text-6xl'>Whats up!!</h1>
-      <div>
-        <div></div>
+    <div className='w-screen h-screen overflow-hidden flex justify-center items-center relative'>
+      <Image src={HeroImage} alt='Hero' fill className='absolute' />
+      <div className='relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm'>
+        <Logo />
+        <p>
+          The AI-powered SEO-optimizer for blog posts. Get high-quality content,
+          without sacrificing your time.
+        </p>
+        <Link href='/post/new' className='btn text-center'>
+          Begin
+        </Link>
       </div>
     </div>
   );
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,11 +1,43 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { faBrain } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { AppLayout } from '../../components/AppLayout';
+// import { getAppProps } from '../../utils/getAppProps';
 
 export default function NewPost(props) {
+  const router = useRouter();
+  const [topic, setTopic] = useState('');
+  const [keywords, setKeywords] = useState('');
+  const [generating, setGenerating] = useState(false);
   console.log(props);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setGenerating(true);
+    try {
+      const response = await fetch(`/api/generatePost`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({ topic, keywords }),
+      });
+      const json = await response.json();
+      console.log('RESULT: ', json);
+      if (json?.postId) {
+        router.push(`/post/${json.postId}`);
+      }
+    } catch (e) {
+      setGenerating(false);
+    }
+  };
   return (
     <div>
       <h1 className='text-6xl'>Whats newpostup!!</h1>
+      <button className='btn' onClick={handleSubmit}>
+        Generate
+      </button>
     </div>
   );
 }
